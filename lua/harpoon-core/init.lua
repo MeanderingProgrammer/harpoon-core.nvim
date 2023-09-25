1417f7e8eefd0a80c3d9fd3dd79e43bf73251cef
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-25 13:52:44 -0700

    Start implementing some of the autocommand logic in init, add empty mark and ui files

--- lua/harpoon-core/init.lua
+++ lua/harpoon-core/init.lua
@@ -1,4 +1,34 @@
+-- Typically resolves to ~/.local/share/nvim
+local user_marks = vim.fn.stdpath('data') .. '/harpoon-core.json'
+
 local M = {}
+
+local group = vim.api.nvim_create_augroup('HarpoonCore', { clear = true })
+
+--vim.api.nvim_create_autocmd({ 'BufLeave, VimLeave' }, {
+--    group = group,
+--    callback = function()
+--        require('harpoon-core.mark').store_offset()
+--    end,
+--})
+
+local function set_open_keymap(key, command)
+    vim.keymap.set('n', key, function()
+        vim.cmd(command)
+        local line = vim.api.nvim_get_current_line()
+        vim.cmd('e ' .. vim.fn.getcwd() .. '/' .. line)
+    end, { buffer = true, noremap = true, silent = true })
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'harpoon-core',
+    group = group,
+    callback = function()
+        set_open_keymap('<C-V>', 'vs')
+        set_open_keymap('<C-x>', 'sp')
+        set_open_keymap('<C-t>', 'tabnew')
+    end,
+})
 
 M.setup = function(opts)
     -- TODO
