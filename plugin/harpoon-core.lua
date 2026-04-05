cac70174729a32e029c12add497168545eb4a3d0
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2026-04-04 19:13:08 -0700

    chore: avoid reloading plugin file by using global

--- plugin/harpoon-core.lua
+++ plugin/harpoon-core.lua
@@ -1,3 +1,8 @@
+if vim.g.loaded_harpoon_core then
+    return
+end
+vim.g.loaded_harpoon_core = true
+
 vim.api.nvim_create_autocmd({ 'BufLeave', 'VimLeave' }, {
     group = vim.api.nvim_create_augroup('HarpoonCore', {}),
     callback = function()
